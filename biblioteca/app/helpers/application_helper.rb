1d4503ff3e35968b3827020b99e9ad42f7aa1de8
Renato Filho <renatosousafilho@gmail.com>
2017-07-15 17:50:47 -0300

    aplicando I18n ao projeto

--- biblioteca/app/helpers/application_helper.rb
+++ biblioteca/app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def class_active
+    "active"
+  end
+
+  def active_home
+    class_active if controller.controller_name == "home"
+  end
+
+  def active_authors
+    class_active if controller.controller_name == "authors"
+  end
+
+  def active_books
+    class_active if controller.controller_name == "books"
+  end
 end
--- biblioteca/app/models/author.rb
+++ biblioteca/app/models/author.rb
@@ -1,7 +1,6 @@
 class Author < ApplicationRecord
   has_many :books
 
-  validates_presence_of :name, message: "O nome não pode ser vazio"
-  validates_presence_of :birthdate, message: "A data de nascimento não pode ser vazio"
-  # validates_uniqueness_of :name, message: "O autor já foi cadastrado"
+  validates_presence_of :name, :birthdate
+  validates_uniqueness_of :name
 end
