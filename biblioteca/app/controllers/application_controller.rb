4117cce8d00e4e09c9dcd71f606ed84bf67409aa
Renato Filho <renatosousafilho@gmail.com>
2017-08-12 15:34:43 -0300

    Adicionando botao de sair e refatorando o uso do callback authenticate_user

--- biblioteca/app/controllers/application_controller.rb
+++ biblioteca/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
+  before_action :authenticate_user!
 
   def set_locale
     I18n.locale = params[:lang] || I18n.default_locale
--- biblioteca/app/controllers/authors_controller.rb
+++ biblioteca/app/controllers/authors_controller.rb
@@ -1,5 +1,4 @@
 class AuthorsController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_author, only: [:show, :edit, :update, :destroy]
 
   def index
