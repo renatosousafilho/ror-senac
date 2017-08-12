1c74bde4c9bd93d8f18d1ba99a7e5de6239d9dfd
Renato Filho <renatosousafilho@gmail.com>
2017-08-12 14:54:49 -0300

    adicinando devise ao projeto

--- biblioteca/config/routes.rb
+++ biblioteca/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "authors" => 'authors#index'
   post 'authors' => 'authors#create'
   get 'author/new' => 'authors#new', as: :new_author
--- biblioteca/db/schema.rb
+++ biblioteca/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805182827) do
+ActiveRecord::Schema.define(version: 20170812174408) do
 
   create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -29,6 +29,23 @@ ActiveRecord::Schema.define(version: 20170805182827) do
     t.integer "total_pages"
     t.string "cover"
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "books", "authors"
